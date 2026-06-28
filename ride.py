109f802367f84ffd47e5d115db942f0aec7656de
Waliullah <waliullah.godev@gmail.com>
2026-06-29 02:10:17 +0600

    start_ride

--- ride.py
+++ ride.py
@@ -1,4 +1,7 @@
 
+from datetime import datetime
+    
+    
     
 class Ride:
     def __init__(self, start_location, end_location):
@@ -9,3 +12,13 @@ class Ride:
         self.start_time = None
         self.end_time = None
         self.estimated_fare = None
+        
+        
+    def set_driver(self, driver):
+        self.driver = driver
+        
+    
+    def start_ride(self):
+        self.start_time = datetime.now()
+        
+        
